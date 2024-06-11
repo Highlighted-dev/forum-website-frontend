1aa310501d14f8aa4f7c75ef9bc6e3d4ddf92d4d
Highlighted-dev <bartosz.korz@gmail.com>
2024-06-11 23:00:49 +0200

    Added tooltip to display number of posts in ProfileCard

--- components/profile/ProfileCard.tsx
+++ components/profile/ProfileCard.tsx
@@ -14,13 +14,23 @@ import { HeartIcon, TextIcon, UploadIcon } from "lucide-react";
 import { Input } from "../ui/input";
 import { IUser } from "@/@types/next-auth";
 import EditProfileForm from "./EditProfileForm";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+import { Label } from "../ui/label";
 
 export default function ProfileCard({
   session,
   user,
 }: {
   session: Session;
-  user: IUser;
+  user: IUser & {
+    numberOfDiscussions: number;
+    numberOfReplies: number;
+  };
 }) {
   const [editing, setEditing] = useState(false);
   return (
@@ -32,15 +42,11 @@ export default function ProfileCard({
               <img src={user.image ?? ""} />
               <AvatarFallback>User</AvatarFallback>
             </Avatar>
-            <h2 className="text-2xl font-bold">
-              {session.user.name ?? "Username"}
-            </h2>
+            <h2 className="text-2xl font-bold">{user.name ?? "Username"}</h2>
           </div>
           <div className="mb-4">
             <h3 className="text-lg font-semibold ">Bio</h3>
-            <p className="text-sm text-gray-300">
-              {session.user.bio ?? "No bio set"}
-            </p>
+            <p className="text-sm text-gray-300">{user.bio ?? "No bio set"}</p>
           </div>
           <Button className="mb-4" onClick={() => setEditing(!editing)}>
             Edit Profile
@@ -56,19 +62,35 @@ export default function ProfileCard({
       <div className="grid grid-cols-2 gap-4 mb-3">
         <Card>
           <CardHeader>
-            <CardTitle>Posts</CardTitle>
-            <CardDescription>5</CardDescription>
+            <CardTitle>
+              {" "}
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>Posts</TooltipTrigger>
+                  <TooltipContent>
+                    <Label>
+                      Number of created discussions + number of replies
+                    </Label>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </CardTitle>
+            <CardDescription>
+              {user.numberOfDiscussions + user.numberOfReplies}
+            </CardDescription>
           </CardHeader>
         </Card>
         <Card>
           <CardHeader>
-            <CardTitle>Rank</CardTitle>
-            <CardDescription>not set</CardDescription>
+            <CardTitle>Role</CardTitle>
+            <CardDescription>{user.role}</CardDescription>
           </CardHeader>
         </Card>
       </div>
       <div>
-        <h3 className="text-lg font-semibold mb-2">Recent Activity</h3>
+        <h3 className="text-lg font-semibold mb-2">
+          Recent Activity (Coming soon!)
+        </h3>
         <div className="space-y-2">
           <div className="flex items-center space-x-2">
             <TextIcon className="w-5 h-5 text-gray-300" />
